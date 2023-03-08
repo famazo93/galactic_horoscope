759d921ae5673280dc535052314b86710ba549b3
famazo <mfarkas@ehrenfeld.hu>
2023-03-08 09:58:37 +0100

    add fade-in animations and full-page cover

---
+++ animations.js
@@ -0,0 +1,18 @@
+const reveal = () => {
+    const reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+      const windowHeight = window.innerHeight;
+      const elementTop = reveals[i].getBoundingClientRect().top;
+      const elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
